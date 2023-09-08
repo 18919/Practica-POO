10a0b47d0365e41cef563ff53cb66ecdd91fee3e
Zaid Cano <78230211+18919@users.noreply.github.com>
2023-09-07 22:21:44 -0600

    Programas de la Practica 1 POO

---
+++ Practica 1/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Cano Vazquez Axel Zaid
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo desde POO grupo 8");
+        System.out.println("XDDDDD");
+        //x: ese simbolo nos indica que el parametro x 
+        //se esta llenando con la cadena que ingresamos
+    }
+    
+}
